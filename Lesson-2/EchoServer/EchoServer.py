c7533676b42cdaf5ca98867751b68ba9bb20e4a7
Karl Krueger <karl@udacity.com>
2017-01-17 12:26:53 -0800

    Improve comments on echo server.

--- Lesson-2/EchoServer/EchoServer.py
+++ Lesson-2/EchoServer/EchoServer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 #
-# The *echo server* is an HTTP server that responds to a GET query by sending
-# the query path back to the client.  For instance, if you go to the URI
-# "http://localhost:8000/Balloon", the echo server will respond with the
-# text "Balloon" in the HTTP response.
+# The *echo server* is an HTTP server that responds to a GET request by
+# sending the query path back to the client.  For instance, if you go to
+# the URI "http://localhost:8000/Balloon", the echo server will respond
+# with the text "Balloon" in the HTTP response body.
 #
 # The starter code for this exercise is the code from the hello server.
-# Your assignment is to change this code into the echo server.
+# Your assignment is to change this code into the echo server:
+#   1. Change the name of the handler from HelloHandler to EchoHandler.
+#   2. Change the response body from "Hello, HTTP!" to the query path.
 #
 # When you're done, run it in your terminal.  Try it out from your browser,
 # then run the "test.py" script to check your work.
