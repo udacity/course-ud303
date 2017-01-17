51624d37a5e63be5b0d86892ca5b92fa28aee1b7
Karl Krueger <karl@udacity.com>
2017-01-17 11:52:27 -0800

    EchoServer starter code, solution, and test code.

---
+++ Lesson-2/EchoServer/EchoServer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#
+# The *echo server* is an HTTP server that responds to a GET query by sending
+# the query path back to the client.  For instance, if you go to the URI
+# "http://localhost:8000/Balloon", the echo server will respond with the
+# text "Balloon" in the HTTP response.
+#
+# The starter code for this exercise is the code from the hello server.
+# Your assignment is to change this code into the echo server.
+#
+# When you're done, run it in your terminal.  Try it out from your browser,
+# then run the "test.py" script to check your work.
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class HelloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # First, send a 200 OK response.
+        self.send_response(200)
+
+        # Then send headers.
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+
+        # Now, write the response body.
+        self.wfile.write("Hello, HTTP!\n".encode())
+
+if __name__ == '__main__':
+    server_address = ('', 8000)  # Serve on all addresses, port 8000.
+    httpd = HTTPServer(server_address, HelloHandler)
+    httpd.serve_forever()
+
---
+++ Lesson-2/EchoServer/test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#
+# Test script for the echo server.
+
+import requests, random
+
+words = ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf",
+         "Hotel", "India", "Juliet", "Kilo", "Lima", "Mike", "November",
+         "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform",
+         "Victor", "Whiskey", "Xray", "Yankee", "Zulu"]
+
+query = "".join(random.choice(words) for _ in range(3))
+uri = "http://localhost:8000/" + query
+print("Sending query for:", uri)
+try:
+  r = requests.get(uri)
+  output = r.text.strip()
+  if r.status_code != 200:
+    print("The server returned status code {}"
+          "instead of a 200 OK.".format(r.status_code))
+  elif output == query:
+    print("Looks good!")
+  else:
+    print("The server sent a 200 OK response, but it wasn't an echo.")
+    print("I expected '{}', but the server said '{}'".format(query, output))
+    if output == "Hello, HTTP!":
+      print("That looks like the hello server talking.")
+except requests.ConnectionError:
+  print("Couldn't connect to the server. Is it running on port 8000?")
+except requests.RequestException:
+  print("Couldn't communicate with the server. Take a look at its output.")
+
