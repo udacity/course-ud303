f933b9674a64ce04d098d06e56cc0764855b0048
Karl Krueger <karl@udacity.com>
2017-01-17 16:28:18 -0800

    Solution code for full Messageboard.

---
+++ Lesson-2/MessageboardPartTwo/solution/MessageboardPartTwo.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+#
+# An HTTP server that's a message board.
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import parse_qs
+
+memory = []
+
+form = '''<!DOCTYPE html>
+  <title>Message Board</title>
+  <form method="POST">
+    <textarea name="message"></textarea>
+    <br>
+    <button type="submit">Post it!</button>
+  </form>
+  <pre>
+{}
+  </pre>
+'''
+
+class MessageHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        # How long was the message?
+        length = int(self.headers.get('Content-length', 0))
+
+        # Read and parse the message
+        data = self.rfile.read(length).decode()
+        message = parse_qs(data)["message"][0]
+
+        # Escape HTML tags in the message so users can't break world+dog.
+        message = message.replace("<", "&lt;")
+
+        # Store it in memory.
+        memory.append(message)
+
+        # Send a 303 back to the root page
+        self.send_response(303)  # redirect via GET
+        self.send_header('Location', '/')
+        self.end_headers()
+
+    def do_GET(self):
+        # First, send a 200 OK response.
+        self.send_response(200)
+
+        # Then send headers.
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+
+        # Send the form with the messages in it.
+        mesg = form.format("\n".join(memory))
+        self.wfile.write(mesg.encode())
+
+if __name__ == '__main__':
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, MessageHandler)
+    httpd.serve_forever()
