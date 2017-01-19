9db7acdda61c3f5c271d7fbdd713c4af9c7e8f00
Karl Krueger <karl@udacity.com>
2017-01-19 12:58:16 -0800

    Messageboard exercise, part two.

---
+++ Lesson-2/4_MessageboardPartTwo/MessageboardPartTwo.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+#
+# Step two in building the messageboard server:
+# A server that handles both GET and POST requests.
+#
+# Instructions:
+#
+# 1. Add a string variable that contains the form from Messageboard.html.
+# 2. Add a do_GET method that returns the form.
+# 
+# You don't need to change the do_POST method in this exercise!
+#
+# To test your code, run this server and access it at http://localhost:8000/
+# in your browser.  You should see the form.  Then put a message into the
+# form and submit it.  You should then see the message echoed back to you.
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import parse_qs
+
+class MessageHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        # How long was the message?
+        length = int(self.headers.get('Content-length', 0))
+        
+        # Read the correct amount of data from the request.
+        data = self.rfile.read(length).decode()
+
+        # Extract the "message" field from the request data.
+        message = parse_qs(data)["message"][0]
+        
+        # Send the "message" field back as the response.
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(message.encode())
+
+if __name__ == '__main__':
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, MessageHandler)
+    httpd.serve_forever()
---
+++ Lesson-2/4_MessageboardPartTwo/README.md
@@ -0,0 +1,8 @@
+# Messageboard, Part Two
+
+In this exercise, you'll give the messageboard server the ability to serve
+its own form. A GET request to the server will show the form, while a POST
+request (submitting the form) will echo back the message.
+
+See `MessageboardPartTwo.py` for the starter code.
+
