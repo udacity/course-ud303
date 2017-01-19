43d283d95ad80d4adc6f48792b1d4e6fb19b4060
Karl Krueger <karl@udacity.com>
2017-01-18 17:53:53 -0800

    Create starter code for UINames.

--- Lesson-2/5_UsingJSON/UINames.py
+++ Lesson-2/5_UsingJSON/UINames.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 #
 # Client for the UINames.com service.
+#
+# 1. Decode the JSON data returned by the UINames.com API.
+# 2. Print the fields in the specified format.
+#
+# Example output:
+# My name is Tyler Hudson and the PIN on my card is 4840.
 
 import requests
 r = requests.get("http://uinames.com/api?ext&region=United%20States",
                  timeout=2.0)
-j = r.json()
+# 1. Add something here!
 
 print("My name is {} {} and the PIN on my card is {}.".format(
-  j["name"],
-  j["surname"],
-  j["credit_card"]["pin"]
+# 2. Add something here!
 ))
