b575d31a91019d7f305c40d6f2d73b3629f8ed5d
Karl Krueger <karl@udacity.com>
2017-01-27 13:23:32 -0800

    pep8-ify BookmarkServer solution code

--- Lesson-2/7_BookmarkServer/solution/BookmarkServer.py
+++ Lesson-2/7_BookmarkServer/solution/BookmarkServer.py
@@ -11,15 +11,15 @@ memory = {}
 form = '''<!DOCTYPE html>
 <title>Bookmark Server</title>
 <form method="POST">
-  <label>Long URI:
-    <input name="longuri">
-  </label>
-  <br>
-  <label>Short name:
-    <input name="shortname">
-  </label>
-  <br>
-  <button type="submit">Save it!</button>
+    <label>Long URI:
+        <input name="longuri">
+    </label>
+    <br>
+    <label>Short name:
+        <input name="shortname">
+    </label>
+    <br>
+    <button type="submit">Save it!</button>
 </form>
 <p>URIs I know about:
 <pre>
@@ -27,70 +27,71 @@ form = '''<!DOCTYPE html>
 </pre>
 '''
 
+
 def CheckURI(uri, timeout=5):
-  '''Check whether this URI is reachable, i.e. does it return a 200 OK?'''
-  try:
-    r = requests.get(uri, timeout=timeout)
-    # If the GET request returns, was it a 200 OK?
-    return r.status_code == 200
-  except requests.RequestException:
-    # If the GET request raised an exception, it's not OK.
-    return False
+    '''Check whether this URI is reachable, i.e. does it return a 200 OK?'''
+    try:
+        r = requests.get(uri, timeout=timeout)
+        # If the GET request returns, was it a 200 OK?
+        return r.status_code == 200
+    except requests.RequestException:
+        # If the GET request raised an exception, it's not OK.
+        return False
 
 
 class Shortener(http.server.BaseHTTPRequestHandler):
-  def do_GET(self):
-    # A GET request will either be for / (the root path) or for /some-name.
-    # Strip off the / and we have either empty string or a name.
-    name = unquote(self.path[1:])
+    def do_GET(self):
+        # A GET request will either be for / (the root path) or for /some-name.
+        # Strip off the / and we have either empty string or a name.
+        name = unquote(self.path[1:])
 
-    if name:
-      if name in memory:
-        # We know that name! Send a redirect to it.
-        self.send_response(303)
-        self.send_header('Location', memory[name])
-        self.end_headers()
-      else:
-        # We don't know that name! Send a 404 error.
-        self.send_response(404)
-        self.send_header('Content-type', 'text/plain; charset=utf-8')
-        self.end_headers()
-        self.wfile.write("I don't know '{}'.".format(name).encode())
-    else:
-      # Root path. Send the form.
-      self.send_response(200)
-      self.send_header('Content-type', 'text/html')
-      self.end_headers()
-      # List the known associations in the form.
-      known = "\n".join("{} : {}".format(key, memory[key]) 
-                        for key in sorted(memory.keys()))
-      self.wfile.write(form.format(known).encode())
+        if name:
+            if name in memory:
+                # We know that name! Send a redirect to it.
+                self.send_response(303)
+                self.send_header('Location', memory[name])
+                self.end_headers()
+            else:
+                # We don't know that name! Send a 404 error.
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain; charset=utf-8')
+                self.end_headers()
+                self.wfile.write("I don't know '{}'.".format(name).encode())
+        else:
+            # Root path. Send the form.
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            # List the known associations in the form.
+            known = "\n".join("{} : {}".format(key, memory[key])
+                              for key in sorted(memory.keys()))
+            self.wfile.write(form.format(known).encode())
 
-  def do_POST(self):
-    # Decode the form data.
-    length = int(self.headers.get('Content-length', 0))
-    body = self.rfile.read(length).decode()
-    params = parse_qs(body)
-    longuri = params["longuri"][0]
-    shortname = params["shortname"][0]
+    def do_POST(self):
+        # Decode the form data.
+        length = int(self.headers.get('Content-length', 0))
+        body = self.rfile.read(length).decode()
+        params = parse_qs(body)
+        longuri = params["longuri"][0]
+        shortname = params["shortname"][0]
 
-    if CheckURI(longuri):
-      # Remember this URI under the specified name.
-      memory[shortname] = longuri
+        if CheckURI(longuri):
+            # This URI is good!  Remember it under the specified name.
+            memory[shortname] = longuri
 
-      # Serve a redirect to the form.
-      self.send_response(303)
-      self.send_header('Location', '/')
-      self.end_headers()
-    else:
-      # Didn't successfully fetch the long URI.
-      self.send_response(404)
-      self.send_header('Content-type', 'text/plain; charset=utf-8')
-      self.end_headers()
-      self.wfile.write(
-          "Couldn't fetch URI '{}'. Sorry!".format(longuri).encode())
+            # Serve a redirect to the form.
+            self.send_response(303)
+            self.send_header('Location', '/')
+            self.end_headers()
+        else:
+            # Didn't successfully fetch the long URI.
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(
+                "Couldn't fetch URI '{}'. Sorry!".format(longuri).encode())
 
 if __name__ == '__main__':
-  server_address = ('', 8000)
-  httpd = http.server.HTTPServer(server_address, Shortener)
-  httpd.serve_forever()
+    server_address = ('', 8000)
+    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd.serve_forever()
