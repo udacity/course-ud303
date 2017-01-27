29032e10f0a0a09f040781bdbe32dadd0e6be30b
Karl Krueger <karl@udacity.com>
2017-01-27 13:37:59 -0800

    Starter code for BookmarkServer.

---
+++ Lesson-2/7_BookmarkServer/BookmarkServer.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+#
+# A *bookmark server* or URI shortener that maintains a mapping (dictionary)
+# between short names and long URIs, checking that each new URI added to the
+# mapping actually works (i.e. returns a 200 OK).
+#
+# This server is intended to serve three kinds of requests:
+#
+#   * A GET request to the / (root) path.  The server returns a form allowing
+#     the user to submit a new name/URI pairing.  The form also includes a
+#     listing of all the known pairings.
+#   * A POST request containing "longuri" and "shortname" fields.  The server
+#     checks that the URI is valid (by requesting it), and if so, stores the
+#     mapping from shortname to longuri in its dictionary.  The server then
+#     redirects back to the root path.
+#   * A GET request whose path contains a short name.  The server looks up
+#     that short name in its dictionary and redirects to the corresponding
+#     long URI.
+#
+# Your job in this exercise is to finish the server code.
+#
+# Here are the steps you need to complete:
+#
+# 1. Write the CheckURI function, which takes a URI and returns True if a
+#    request to that URI returns a 200 OK, and False otherwise.
+#
+# 2. Write the code inside do_GET that sends a 303 redirect to a known name.
+#
+# 3. Write the code inside do_POST that sends a 303 redirect to the form
+#    after saving a newly submitted URI.
+#
+# 4. Write the code inside do_POST that sends a 404 error if a URI is not
+#    successfully checked (i.e. if CheckURI returns false).
+#
+# In each step, you'll need to delete a line of code that raises the
+# NotImplementedError exception.  These are there as placeholders in the
+# starter code.
+#
+# After writing each step, restart the server and run test.py to test it.
+
+import http.server
+import requests
+from urllib.parse import unquote, parse_qs
+
+memory = {}
+
+form = '''<!DOCTYPE html>
+<title>Bookmark Server</title>
+<form method="POST">
+    <label>Long URI:
+        <input name="longuri">
+    </label>
+    <br>
+    <label>Short name:
+        <input name="shortname">
+    </label>
+    <br>
+    <button type="submit">Save it!</button>
+</form>
+<p>URIs I know about:
+<pre>
+{}
+</pre>
+'''
+
+
+def CheckURI(uri, timeout=5):
+    '''Check whether this URI is reachable, i.e. does it return a 200 OK?
+
+    This function returns True if a GET request to uri returns a 200 OK, and
+    False if that GET request returns any other response, or doesn't return
+    (i.e. times out).
+    '''
+    # 1. Write this function.  Delete the following line.
+    raise NotImplementedError("Step 1 isn't written yet.")
+
+
+class Shortener(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        # A GET request will either be for / (the root path) or for /some-name.
+        # Strip off the / and we have either empty string or a name.
+        name = unquote(self.path[1:])
+
+        if name:
+            if name in memory:
+                # 2. Send a 303 redirect to the long URI in memory[name].
+                #    Delete the following line.
+                raise NotImplementedError("Step 2 isn't written yet.")
+            else:
+                # We don't know that name! Send a 404 error.
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain; charset=utf-8')
+                self.end_headers()
+                self.wfile.write("I don't know '{}'.".format(name).encode())
+        else:
+            # Root path. Send the form.
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            # List the known associations in the form.
+            known = "\n".join("{} : {}".format(key, memory[key])
+                              for key in sorted(memory.keys()))
+            self.wfile.write(form.format(known).encode())
+
+    def do_POST(self):
+        # Decode the form data.
+        length = int(self.headers.get('Content-length', 0))
+        body = self.rfile.read(length).decode()
+        params = parse_qs(body)
+        longuri = params["longuri"][0]
+        shortname = params["shortname"][0]
+
+        if CheckURI(longuri):
+            # This URI is good!  Remember it under the specified name.
+            memory[shortname] = longuri
+
+            # 3. Serve a redirect to the root page (the form).
+            #    Delete the following line.
+            raise NotImplementedError("Step 3 isn't written yet!")
+        else:
+            # Didn't successfully fetch the long URI.
+
+            # 4. Send a 404 error with a useful message.
+            #    Delete the following line.
+            raise NotImplementedError("Step 4 isn't written yet!")
+
+if __name__ == '__main__':
+    server_address = ('', 8000)
+    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd.serve_forever()
